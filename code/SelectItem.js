16589122662f708f67d314b61c7eb7709d745520
Siamak Hodjat <shodjat@viv.ai>
2019-06-17 22:27:03 -0700

    support selecting a shirt by size

--- code/SelectItem.js
+++ code/SelectItem.js
@@ -2,13 +2,13 @@ var fail = require('fail')
 var lib = require("./lib/util.js");
 
 // SelectItem
-exports.function = function(searchTerm, items) {
+exports.function = function(searchTerm, sizeSearchTerm, items) {
    items = [].concat(items)
    if (items.length == 1 && !searchTerm) {
      var selected = items[0]
      return selected
    }
-   var matches = lib.findItems(items, searchTerm)
+   var matches = lib.findItems(items, searchTerm, sizeSearchTerm)
    if (matches.length == 1) {
      return matches
    } else if (matches.length > 1) {
