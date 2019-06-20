0bf3e0463b2764196acc9e79bba5f863195fcdee
Siamak Hodjat <shodjat@viv.ai>
2019-06-20 15:21:30 -0700

    comment

--- code/CreateItems.js
+++ code/CreateItems.js
@@ -5,11 +5,11 @@ exports.function = function(item, item2, item3) {
       items.push(item)
     }
     if (item2) {
-      delete item2.$type
+      delete item2.$type  //delete type so Item1 can be type casted to an Item
       items.push(item2)
     }
     if (item3) {
-      delete item3.$type
+      delete item3.$type  //delete type so Item1 can be type casted to an Item
       items.push(item3)
     }
     return {items: items}
